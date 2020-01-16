68d1c7624e354fcc9f5af076f89ceb6ed2cc76a4
tang <541839425@qq.com>
2020-01-16 20:25:48 +0800

    tst

--- src/com/woniuxy/ch1231/Text.java
+++ src/com/woniuxy/ch1231/Text.java
@@ -2,7 +2,7 @@ package com.woniuxy.ch1231;
 @times(name = "admin", num = { 2 }, number = 5)
 public class Text {
 	public static void main(String[] args) {
-		
+		System.out.println("ergtewrtew");
 	}
 	
 }
